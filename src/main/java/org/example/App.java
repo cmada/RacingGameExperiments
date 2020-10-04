26c40a2a5e1ef8152a119527f32e3d9e227c6f88
cmada <madalina.ciceu@gmail.com>
2020-10-04 17:18:05 +0300

    Initial commit (test)

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world! This is a test for completing the course.
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
